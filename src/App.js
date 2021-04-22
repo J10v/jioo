ded58b6384ecbcc78cc07b6ba95a5949ee6a70b1
Jiovanni <jiovanni554@gmail.com>
2021-04-21 17:24:50 -0700

    navbar complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <h2>Portfolio </h2>
-  );
+  return <Navbar />;
 }
 
 export default App;
