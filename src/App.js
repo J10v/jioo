3ad2bce158c5a3dcea20757d33babd13a53afb68
J10v <68559232+J10v@users.noreply.github.com>
2021-04-22 23:14:51 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Navbar from "./components/Navbar";
-
+import Header from "./components/Header";
 function App() {
-  return <Navbar />;
+  return (
+    <>
+  <Navbar />
+  <Header />
+  </>
+  );
 }
 
 export default App;
