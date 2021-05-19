663f2c3fa6a1620a43d65c2ed44b6623fcb737eb
J10v <68559232+J10v@users.noreply.github.com>
2021-05-18 19:34:52 -0700

    Add files via upload

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,31 @@
-import React from "react";
-import p1 from "../p1.jpg";
+import React from 'react'
+import photo from "./p.jpg";
+
 
 function About() {
   return (
-    <div className=  "about-section" id="about">
-      <div className="col-6 col-sm-3 text-center">
-      <h1> About Jiovanni </h1>
-      <hr />
-      <h3>
-        {" "}
-        I am a web developer in traning and recent Coding bootcamp (Nucamp)
-        graduate and this is my personal website{" "}
-      </h3>
-      
+    <div className="about" id="about">
+      <div class="max-width">
+        <h2 class="title">About Me</h2>
+        <div className="about-content">
+          <div className="colum left">
+            <img src={photo} alt="">
+            </img>
+          </div>
+          <div className="colum right">
+            <div class="text"> im jio </div>
+            <p>loremfvdfvjsdfnmviedmv fiovmemvsfjkmv fvkjmndfkmdejfnckjd vckjfvnjfd vckj console.assert(vfj vj f
+              jfnvcjdsnvckjdfs v kfjnvldjfs vcjdsf jf vcjansdckjnasdclk, object)</p>
+          </div>
+        </div>
       </div>
+      <section>
+
+      </section>
     </div>
   );
 }
 
-export default About;
+export default About
+
+
